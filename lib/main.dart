aab01974f0db900ff5f0aad3b11452f227fcdc14
abhishekgupta-mset <abhishek.gupta@mset.io>
2023-10-31 02:04:17 +0530

    Implemented tip calculation flow

--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,151 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    return buildMaterialApp(context);
+  }
+
+  MaterialApp buildMaterialApp(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        theme: ThemeData(primarySwatch: Colors.orange),
+        home: Scaffold(
+            appBar: AppBar(title: const Text("Tip Calculator")),
+            body: const TipCalculatorScreen()));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TipCalculatorScreen extends StatefulWidget {
+  const TipCalculatorScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TipCalculatorScreen> createState() => _TipCalculatorScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _TipCalculatorScreenState extends State<TipCalculatorScreen> {
+  var tip = 15.00;
+  var total = 0.00;
+  var sliderValue = 15.0;
+  var color = Colors.orange;
+  var billAmount = "0.0";
+  var reaction = "";
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return _buildTipCalculateScreen(context);
+  }
+
+  Widget _buildTipCalculateScreen(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text("Enter bill amount:"),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: TextField(
+                keyboardType: TextInputType.number,
+                onChanged: (value) => setState(() {
+                      billAmount = value;
+                      calculateTip(value as double);
+                    })),
+          ),
+          const Text("Select tip percentage"),
+          Slider(
+              value: sliderValue,
+              label: "${sliderValue.round()}%",
+              divisions: 10,
+              min: 0.0,
+              max: 30.0,
+              activeColor: color,
+              inactiveColor: Colors.grey,
+              secondaryActiveColor: Colors.black,
+              onChanged: (value) => setState(() {
+                    sliderValue = value;
+                    color = setColor(sliderValue);
+                    reaction = setEmoji(sliderValue);
+                    tip = value;
+                    calculateTip(sliderValue);
+                  })),
+          Text("Tip PKR: ${tip.toStringAsFixed(2)}%"),
+          Text("Total PKR: ${total.toStringAsFixed(2)}%"),
+          Text(reaction, style: const TextStyle(fontSize: 50.0))
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void calculateTip(double tipAmount) {
+    var bill = double.parse(billAmount);
+    if (bill > 0) {
+      tip = tipAmount;
+      total = bill + (bill*tip)/100;
+    }
+  }
+
+  MaterialColor setColor(double sliderValue) {
+    switch (sliderValue) {
+      case < 10.0:
+        {
+          return Colors.red;
+        }
+      case > 10.0 && < 20:
+        {
+          return Colors.orange;
+        }
+      case > 20:
+        {
+          return Colors.green;
+        }
+      default:
+        {
+          return Colors.orange;
+        }
+    }
+  }
+
+  String setEmoji(double sliderValue) {
+    switch (sliderValue) {
+      case < 5.0:
+        {
+          return "😈😈😈";
+        }
+      case < 8.0:
+        {
+          return "😈😈";
+        }
+      case < 10.0:
+        {
+          return "😈";
+        }
+      case > 10.0 && < 15:
+        {
+          return "☺️";
+        }
+      case > 10.0 && < 18:
+        {
+          return "☺️ 😊";
+        }
+      case > 10.0 && < 20:
+        {
+          return "☺️ 😊 😇";
+        }
+      case > 20.0 && < 23:
+        {
+          return "😍";
+        }
+      case > 20.0 && < 25:
+        {
+          return "😍 😻 ";
+        }
+      case 30:
+        {
+          return "☺️ 😊 🥰 🥰";
+        }
+      default:
+        {
+          return "";
+        }
+    }
   }
 }
