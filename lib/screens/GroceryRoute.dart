e9628153d99a69b00a4a4b7ba84ef256a73548ec
abhishekgupta-mset <abhishek.gupta@mset.io>
2023-11-14 16:57:11 +0530

    Added bottomsheet

--- lib/screens/GroceryRoute.dart
+++ lib/screens/GroceryRoute.dart
@@ -3,27 +3,54 @@ import 'package:flutter/material.dart';
 import 'package:tip_calculator/screens/ProfileScreen.dart';
 
 class GroceryRoute extends StatelessWidget {
-
-
   const GroceryRoute({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Grocery Route'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Open Profile'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const ProfileScreen()),
-            );          },
+        appBar: AppBar(
+          title: const Text('Grocery Route'),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            ElevatedButton(
+              child: const Text('Open Profile'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ProfileScreen()),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: const Text('showModalBottomSheet'),
+              onPressed: () {
+                showModalBottomSheet<void>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Container(
+                      height: 200,
+                      color: Colors.amber,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            const Text('Modal BottomSheet'),
+                            ElevatedButton(
+                              child: const Text('Close BottomSheet'),
+                              onPressed: () => Navigator.pop(context),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ]),
+        ));
   }
 }
-
